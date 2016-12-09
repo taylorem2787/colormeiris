f23c62387c07591dd4636a93fb2edc8ca0837b50
Taylor Emolo <tayloremolo@tay.local>
2016-12-08 21:11:11 -0500

    created

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# colormeiris
+#<b>color</b>me
+
+
+<b>color</b>me by iris 
+</br>
+All photos sourced through <a href="www.pexels.com">Pexels</a>.
+
+
+-----------
+
+###What is it?
+-----------
+  
+<b>color</b>me is a photography rating app based
+on color preferences. It finds your favorite color 
+through a series of photography rating preferences, 
+to help you get closer to your true personality. 
+
+
+###Where to find it?
+------------
+
+<b>color</b>me can be found at: https://irisphoto.herokuapp.com/
+
+
+###How was it made? 
+---------
+
+Libraries and Packages:
+</br> o JQuery
+</br> o Materialize
+</br> o Particles.js
+</br> o Dominant Color
+</br> o MySQL
+</br> o Express
+</br> o Body Parser
+</br> o Path
+
+Resources: 
+</br> o Pexels.com
+
+
+###Contacts
+--------
+
+ o If you have a concrete bug report for <b>color</b>me 
+ please email <iris.technical@gmail.com> 
+
+ o If you want to participate in actively developing 
+ <b>color</b>me please email <iris.dev@gmail.com>
---
+++ server.js
@@ -0,0 +1,55 @@
+//Imported modules
+//============================================================
+//npm server package
+var express = require('express');
+//npm package used for 
+var bodyParser = require('body-parser');
+//npm package to handle file pathways
+var path = require('path');
+//module to establish mysql connection with Jaws DB
+var connection = require('./config/connection.js');
+//npm module 'dot-env'; deal with sensitive info
+require('dotenv').config();
+
+//Run express app
+//================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+//App listener
+app.listen(PORT, function(){
+	console.log('Listening on port: ' + PORT);
+});
+
+//Express middleware for parsing info for http POST requests
+//================================================
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+//establish connection to mysql/jawsdb
+connection.connect(function(err){
+	if (err){
+		console.error(err.stack);
+	}
+	console.log('connected as ID ' + connection.threadId);
+});
+
+
+//Get route for the profile page
+//================================================
+app.get('/profile/:user', function(req, res){
+	var user = req.params.user;
+	var queryString = `SELECT * FROM ` + user;
+	connection.query(queryString, function(err, data){
+		res.send(data);
+	});
+});
+
+//External routing files
+//================================================
+require('./api/api-routes.js')(app);
+require('./api/admin-routes.js')(app);
+require('./api/login-register-routes.js')(app);
+require('./api/static-file-routes.js')(app);
+
